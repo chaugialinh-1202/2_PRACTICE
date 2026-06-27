1aad54aed62d5445299361a740875ad5c0403e1a
chaugialinh-1202 <chaugialinh1202@gmail.com>
2026-06-27 12:11:43 +0700

    submit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let btn = document.getElementById("btn-click")
-let btn_alert = btn.addEventListener("click",function(e){
-    alert("Button Clicked!")
-    console.log(e);
+let img = document.getElementById("img")
+let btn  = document.getElementById("btn-click")
+let btn_change_img = btn.addEventListener("click",function(e){
+    img.setAttribute("src","img/2.png")
 })
-
